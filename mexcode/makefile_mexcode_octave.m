2feffa65313f71fe94a7cd02f287d1d35a8949fa
atriveg <161699987+atriveg@users.noreply.github.com>
2026-07-08 12:32:34 +0200

    Update makefile_mexcode_octave.m

--- mexcode/makefile_mexcode_octave.m
+++ mexcode/makefile_mexcode_octave.m
@@ -106,8 +106,8 @@ if(isunix)
             % Use a custom implementation of BLAS and LAPACK
             customblas   = get_custom_BLAS(path0);
             customlapack = get_custom_LAPACK(path0);
-            [p1,~,~]     = fielpartd(customblas);
-            [p2,~,~]     = fielpartd(customlapack);
+            [p1,~,~]     = fielparts(customblas);
+            [p2,~,~]     = fielparts(customlapack);
             blaslinks    = { sprintf('-L%s',p1), sprintf('-L%s',p2), sprintf('-l%s',customblas), sprintf('-l%s',customlapack) };
             blasflags    = {'-D_SYSTEM_BLAS_BUILD_'};
         case 6
